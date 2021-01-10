f3332fcb98f1b257d4233cc4f0351f53544d134e
Lucaswhy <lucas_herculano_2010@hotmail.com>
2021-01-10 00:15:12 -0300

    Exercicio de polimorfismo com herança

---
+++ ExercicioPolimorfismo/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace ExercicioPolimorfismo.Entities {
+    class Product {
+        protected string _name { get; set; }
+        protected double _price { get; set; }
+
+        public Product() {
+
+        }
+
+        public Product(string name, double price) {
+            _name = name;
+            _price = price;
+        }
+
+        public virtual string priceTag() {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"{_name} $ {_price.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            return sb.ToString();
+        }
+
+    }
+}
