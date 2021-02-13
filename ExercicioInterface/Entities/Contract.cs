7e3f667955fd82522ed78efc16005affa89fe3e1
Lucaswhy <lucas_herculano_2010@hotmail.com>
2021-02-13 20:17:16 -0300

    Exercicio utilizando interface.

---
+++ ExercicioInterface/Entities/Contract.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExercicioInterface.Entities {
+    class Contract {
+
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+
+        private List<Installments> _installments { get; set; }
+
+        public Contract(int number, DateTime date, double totalValue) {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+            _installments = new List<Installments>();
+        }
+
+        public void addList(Installments list) {
+            _installments.Add(list);
+        }
+
+        public void removeList(Installments list) {
+            _installments.Remove(list);
+        }
+        
+        public override string ToString() {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Installments:\n");
+            
+            foreach(Installments item in _installments) {
+                sb.Append(item.DueDate.ToString("dd/MM/yyyy"));
+                sb.Append(" - " + item.Amount.ToString("F2",CultureInfo.InvariantCulture) + "\n");
+            }
+
+            return sb.ToString();
+        }
+
+
+    }
+}
---
+++ ExercicioInterface/Entities/Installments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace ExercicioInterface.Entities {
+    class Installments {
+
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installments(DateTime dueDate, double amount) {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+               
+    }
+}
